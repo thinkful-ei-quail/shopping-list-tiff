45c6fb5fda4cac022e166169363e028f6d134b35
Tiffany <tiffanysummerford@gmail.com>
2020-08-04 10:39:10 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+function submit() {
+  $("#submitButton").click(event => {
+  event.preventDefault();
+  console.log('hi')
+  const inputItem = $("shopping-list-entry").val();//storing info
+  if (inputItem != "") {
+    $(".shopping-list").append(`<li>
+            <span class="shopping-item">"${inputItem}"</span>
+            <div class="shopping-item-controls">
+              <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+              </button>
+              <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+              </button>
+            </div>
+          </li>`)
+  }
+})
+};
+
+$(submit);
+
+//toggle list item w/ hidden css
+$(".shopping-item-toggle").click(event => {
+  event.preventDefault();
+  if (String.match('#shopping-item')) {
+    $('#shopping-item').css('.shopping-item_checked')
+  }
+})
+
+//remove or empty list item
+$('.shopping-item-delete').click(event => {
+  event.preventDefault();
+  $('inputItem').empty()
+})
+
+
+  //$("element").method("elementToChange")
+  // ie: $(#class).append(src)
+  // .className
+  // #tagID
