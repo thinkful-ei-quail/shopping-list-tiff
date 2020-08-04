83ed4b087fa74159056f80f71c1c086c484a62f6
Tiffany <tiffanysummerford@gmail.com>
2020-08-04 10:46:09 -0400

    added callback fctn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function submit() {
   $("#submitButton").click(event => {
-  event.preventDefault();
-  console.log('hi')
-  const inputItem = $("shopping-list-entry").val();//storing info
-  if (inputItem != "") {
-    $(".shopping-list").append(`<li>
+    event.preventDefault();
+    console.log('hi')
+    const inputItem = $("shopping-list-entry").val();//storing info
+    if (inputItem != "") {
+      $(".shopping-list").append(`<li>
             <span class="shopping-item">"${inputItem}"</span>
             <div class="shopping-item-controls">
               <button class="shopping-item-toggle">
@@ -15,26 +15,27 @@ function submit() {
               </button>
             </div>
           </li>`)
-  }
-})
-};
+    }
+  })
 
-$(submit);
 
-//toggle list item w/ hidden css
-$(".shopping-item-toggle").click(event => {
-  event.preventDefault();
-  if (String.match('#shopping-item')) {
-    $('#shopping-item').css('.shopping-item_checked')
-  }
-})
+  $(submit);
 
-//remove or empty list item
-$('.shopping-item-delete').click(event => {
-  event.preventDefault();
-  $('inputItem').empty()
-})
 
+  //toggle list item w/ hidden css
+  $(".shopping-item-toggle").click(event => {
+    event.preventDefault();
+    if (String.match('#shopping-item')) {
+      $('#shopping-item').css('.shopping-item_checked')
+    }
+  })
+
+  //remove or empty list item
+  $('.shopping-item-delete').click(event => {
+    event.preventDefault();
+    $('inputItem').empty()
+  })
+}
 
   //$("element").method("elementToChange")
   // ie: $(#class).append(src)
